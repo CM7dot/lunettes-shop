18b709d5c90f61829824265ab3a8660c02a0b538
CM7dot <marveen2001@gmail.com>
2026-03-21 13:39:20 +0100

    ajout animations produits

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,38 +1,43 @@
 import "./Products.css";
 
 function Products() {
+
+  const products = [
+    {
+      name: "Lunettes Eveuss",
+      price: "89€",
+      image: "https://originel-eyewear.com/cdn/shop/files/EVEUSS_LUNETTE_DE_VUE_FEMME_ORIGINEL_750x.jpg?v=1690365406"
+    },
+    {
+      name: "Lunettes Mase",
+      price: "129€",
+      image: "https://originel-eyewear.com/cdn/shop/files/originel-eyewear-france-lunette-de-vue-femme-original-moderne-essayage-virtuel_550x.jpg?v=1694164884"
+    },
+    {
+      name: "Lunettes Canthal",
+      price: "199€",
+      image: "https://originel-eyewear.com/cdn/shop/files/originel-eyewear-france-lunette-de-vue-ecaille-combinee-noir-blanc-moderne-tendance_1445x.jpg?v=1694168552"
+    },
+    {
+      name: "Lunettes Cleare",
+      price: "250€",
+      image: "https://originel-eyewear.com/cdn/shop/files/ORIGINEL_EYEWEAR_FRANCE_LUNETTE_DE_VUE_TRANSPARENTE_TENDANCE_550x.jpg?v=1694103586"
+    }
+  ];
+
   return (
     <section className="products">
       <h2>Tendances</h2>
 
       <div className="product-grid">
-        <div className="product-card">
-          <img src="https://originel-eyewear.com/cdn/shop/files/EVEUSS_LUNETTE_DE_VUE_FEMME_ORIGINEL_750x.jpg?v=1690365406" />
-          <h3>Lunettes Eveuss</h3>
-          <p>89€</p>
-          <button>Acheter</button>
-        </div>
-
-        <div className="product-card">
-          <img src="https://originel-eyewear.com/cdn/shop/files/originel-eyewear-france-lunette-de-vue-femme-original-moderne-essayage-virtuel_550x.jpg?v=1694164884" />
-          <h3>Lunettes Mase</h3>
-          <p>129€</p>
-          <button>Acheter</button>
-        </div>
-
-        <div className="product-card">
-          <img src="https://originel-eyewear.com/cdn/shop/files/originel-eyewear-france-lunette-de-vue-ecaille-combinee-noir-blanc-moderne-tendance_1445x.jpg?v=1694168552" />
-          <h3>Lunettes Canthal</h3>
-          <p>199€</p>
-          <button>Acheter</button>
-        </div>
-
-        <div className="product-card">
-          <img src="https://originel-eyewear.com/cdn/shop/files/ORIGINEL_EYEWEAR_FRANCE_LUNETTE_DE_VUE_TRANSPARENTE_TENDANCE_550x.jpg?v=1694103586" />
-          <h3>Lunettes Cleare</h3>
-          <p>250€</p>
-          <button>Acheter</button>
-        </div>
+        {products.map((product, index) => (
+          <div className="product-card" key={index} style={{ animationDelay: `${index * 0.15}s` }} >
+            <img src={product.image} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <button>Acheter</button>
+          </div>
+        ))}
       </div>
     </section>
   );
